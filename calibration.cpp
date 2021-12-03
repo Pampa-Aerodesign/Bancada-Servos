22fd97fd638b0767245ee5456c159f5f6fbcff13
Luiz Filipe Müller dos Reis <luizfilipemullerdosreis@gmail.com>
2021-12-03 18:21:44 -0300

    Addad comments to calibration and datalogger

--- calibration.cpp
+++ calibration.cpp
@@ -2,13 +2,13 @@
 
 void calibrationStart(Servo *pServo, uint16_t servoCenter, Adafruit_INA219 *pIna219)
 {
-    pServo->write(servoCenter);
-    delay(5000);
-    pServo->detach();
-    pIna219->setCalibration_32V_2A();
+    pServo->write(servoCenter);       // Moves the servo to the center.
+    delay(5000);                      // Wait fot the servo go to the center and disconect the servo if needed.
+    pServo->detach();                 // Detach the servo.
+    pIna219->setCalibration_32V_2A(); // Reset the calibration configuration of the INA219.
 }
 
-void calibrationEnd(Servo *pServo, uint8_t servoPin, Sampling *pSampling, uint16_t smpSize, Adafruit_INA219 *pIna219)
+void calibrationEnd(Servo *pServo, uint8_t servoPin)
 {
-    pServo->attach(servoPin);
+    pServo->attach(servoPin); // Ataches the servo back to its pin.
 }
--- datalogger.cpp
+++ datalogger.cpp
@@ -2,26 +2,28 @@
 
 void Datalogger::begin(char header_[64])
 {
-    startTime = 0;
-    strcpy(header, header_);
+    startTime = 0;           // Set the start time to 0 on the initial configuration.
+    strcpy(header, header_); // Copy the header from the input to the internal variable.
 }
 
 void Datalogger::send(float current, float voltage)
 {
+    // Verify if the start time is zero.
     if (!startTime)
     {
-        Serial.println("header");
-        Serial.println(header);
+        Serial.println("header"); // Print the word header so the python logger knows that the next line is the actual header.
+        Serial.println(header);   // Send the header.
 
-        startTime = millis();
+        startTime = millis(); // Store the current time from the millis() function.
     }
 
+    // In case of the start time is not zero, send the lines of data to the logger.
     else
     {
-        Serial.print(millis() - startTime);
-        Serial.print(",");
-        Serial.print(current);
-        Serial.print(",");
-        Serial.println(voltage);
+        Serial.print(millis() - startTime); // First element is the difference between the current time and the start time.
+        Serial.print(",");                  // Print a comma to separate the information on the lines.
+        Serial.print(current);              // Print the current.
+        Serial.print(",");                  // Another separator.
+        Serial.println(voltage);            // Print the voltage and the line ending.
     }
 }
